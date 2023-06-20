0d36b1612037fad5c3655aa23c1893f39bf7f01c
Rafik Kuliev <rombaks2019@gmail.com>
2023-06-20 21:25:21 +0300

    Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
